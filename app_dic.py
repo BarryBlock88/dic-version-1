1d463585dfd3933d0625b897fe1766f953db5b0a
BarryBlock88 <cool_beans2011@yahoo.com>
2021-05-31 17:28:56 +0000

    verion simple of a dictionary

--- app_dic.py
+++ app_dic.py
@@ -1,13 +1,38 @@
 import json
 
-data = json.load(open("package.json"))
+#load JSON data
+data = json.load(open("diction.json"))
 
-def translate(word):
-    if word in data:
-        return data(word)
+#take word from user
+word = input('Enter word: ')
+
+#function to return meaning of the word from data
+def getMeaning(w):
+    #for case sensitivity
+    w = w.lower()
+    #if-else to check word exist in our data or not
+    if w in data:
+        return data[w]
+    #give matching word
+    elif w not in data:
+        print("Did you mean %s instead? Enter Y if yes or N if no: ")
+        choice = input()
+        choice = choice.lower()
+        if choice == 'y':
+            return data
+        elif choice == 'n':
+            return "The word doesn't exist. Please double check your spelling."
+        else:
+            return "Sorry, We didn't understand your entry."
     else:
-        return "The words not there, double check your spelling"
+        return "The word doesn't exist. Please try again."
 
-word = input("enter word: ")
+#function call to get meaning of the word entered by user
+meaning = getMeaning(word)
 
-print(translate(word))
+#printing meaning of the word in console
+if type(meaning) == list:
+    for item in meaning:
+        print(item)
+else:
+    print(meaning)
